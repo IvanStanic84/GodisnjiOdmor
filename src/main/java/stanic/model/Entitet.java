16dd0bfd7fa3e8b1f51fcfb6570511c9ffe2ccd0
Korisnik <stambaivan@yahoo.com>
2023-01-09 09:01:49 +0100

    model

---
+++ src/main/java/stanic/model/Entitet.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package stanic.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Entitet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer sifra;
+
+    public Entitet() {
+        super();
+    }
+
+    public Entitet(Integer sifra) {
+        super();
+        this.sifra = sifra;
+    }
+
+    public Integer getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(Integer sifra) {
+        this.sifra = sifra;
+    }
+
+}
---
+++ src/main/java/stanic/model/Zaposlenik.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package stanic.model;
+
+import jakarta.persistence.ManyToOne;
+
+/**
+ *
+ * @author Korisnik
+ */
+public class Zaposlenik extends Entitet {
+
+    private String ime;
+    private String prezime;
+    private String oib;
+    private int ukupniBrojDana;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public int getUkupniBrojDana() {
+        return ukupniBrojDana;
+    }
+
+    public void setUkupniBrojDana(int ukupniBrojDana) {
+        this.ukupniBrojDana = ukupniBrojDana;
+    }
+
+    public Godisnji getGodisnji() {
+        return godisnji;
+    }
+
+    public void setGodisnji(Godisnji godisnji) {
+        this.godisnji = godisnji;
+    }
+    @ManyToOne
+    private Godisnji godisnji;
+
+    public Zaposlenik() {
+    }
+
+    public Zaposlenik(String ime, String prezime, String oib, int ukupniBrojDana, Godisnji godisnji) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.oib = oib;
+        this.ukupniBrojDana = ukupniBrojDana;
+        this.godisnji = godisnji;
+    }
+
+    public Zaposlenik(String ime, String prezime, String oib, int ukupniBrojDana, Godisnji godisnji, Integer sifra) {
+        super(sifra);
+        this.ime = ime;
+        this.prezime = prezime;
+        this.oib = oib;
+        this.ukupniBrojDana = ukupniBrojDana;
+        this.godisnji = godisnji;
+    }
+
+}
